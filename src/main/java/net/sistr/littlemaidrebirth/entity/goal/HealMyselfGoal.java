af85ee3aa9459415848d0958de37a9f9e4100fcd
sistr <game.sistr@gmail.com>
2021-03-14 14:58:09 +0900

    砂糖が切れたときにクラッシュするのを修正と、キャッシュ追加

--- src/main/java/net/sistr/littlemaidrebirth/entity/goal/HealMyselfGoal.java
+++ src/main/java/net/sistr/littlemaidrebirth/entity/goal/HealMyselfGoal.java
@@ -1,6 +1,5 @@
 package net.sistr.littlemaidrebirth.entity.goal;
 
-import net.sistr.littlemaidrebirth.entity.InventorySupplier;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.inventory.IInventory;
@@ -8,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
+import net.sistr.littlemaidrebirth.entity.InventorySupplier;
 import net.sistr.lmml.entity.compound.SoundPlayable;
 import net.sistr.lmml.resource.util.LMSounds;
 
@@ -20,6 +20,7 @@ public class HealMyselfGoal<T extends CreatureEntity & InventorySupplier> extend
     private final int healInterval;
     private final int healAmount;
     private int cool;
+    private int cache;
 
     public HealMyselfGoal(T mob, Set<Item> healItems,
                           int healInterval, int healAmount) {
@@ -35,12 +36,12 @@ public class HealMyselfGoal<T extends CreatureEntity & InventorySupplier> extend
         assert mob.getMaxHealth() != 0;
         return (mob.hurtTime <= 0 && mob.getHealth() <= mob.getMaxHealth() - 1
                 || mob.getHealth() / mob.getMaxHealth() < 0.75F)
-                && getHealItemSlot() != -1;
+                && hasHealItem();
     }
 
     @Override
     public boolean shouldContinueExecuting() {
-        return mob.getHealth() < mob.getMaxHealth();
+        return mob.getHealth() < mob.getMaxHealth() && hasHealItem();
     }
 
     @Override
@@ -56,16 +57,12 @@ public class HealMyselfGoal<T extends CreatureEntity & InventorySupplier> extend
             return;
         }
         cool = healInterval;
-        IInventory inventory = this.mob.getInventory();
+
         int slot = getHealItemSlot();
-        ItemStack healItem = inventory.getStackInSlot(slot);
-        if (healItem.isEmpty()) {
-            return;
-        }
-        healItem.shrink(1);
-        if (healItem.isEmpty()) {
-            inventory.removeStackFromSlot(slot);
-        }
+        ItemStack healItem = getHealItem(slot);
+        if (healItem.isEmpty()) return;
+        consumeHealItem(slot, healItem);
+
         mob.heal(healAmount);
         mob.playSound(SoundEvents.ENTITY_ITEM_PICKUP, 1.0F, mob.getRNG().nextFloat() * 0.1F + 1.0F);
         mob.swingArm(Hand.MAIN_HAND);
@@ -78,15 +75,40 @@ public class HealMyselfGoal<T extends CreatureEntity & InventorySupplier> extend
         }
     }
 
+    public boolean hasHealItem() {
+        return getHealItemSlot() != -1;
+    }
+
     public int getHealItemSlot() {
         IInventory inventory = this.mob.getInventory();
+        if (cache != -1) {
+            ItemStack slotStack = inventory.getStackInSlot(cache);
+            if (healItems.contains(slotStack.getItem())) {
+                return cache;
+            } else {
+                cache = -1;
+            }
+        }
         for (int i = 0; i < inventory.getSizeInventory(); i++) {
             ItemStack slotStack = inventory.getStackInSlot(i);
             if (healItems.contains(slotStack.getItem())) {
+                cache = i;
                 return i;
             }
         }
         return -1;
+    }
+
+    public ItemStack getHealItem(int slot) {
+        if (slot == -1) return ItemStack.EMPTY;
+        return this.mob.getInventory().getStackInSlot(slot);
+    }
+
+    public void consumeHealItem(int slot, ItemStack healItem) {
+        healItem.shrink(1);
+        if (healItem.isEmpty()) {
+            this.mob.getInventory().removeStackFromSlot(slot);
+        }
     }
 
 }
